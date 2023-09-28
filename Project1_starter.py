fbbd48660c6f20484afc0c13537e521c13b02411
Douglasmyu <123704099+Douglasmyu@users.noreply.github.com>
2023-09-28 12:21:38 -0700

    Update Project1_starter.py

--- Project1_starter.py
+++ Project1_starter.py
@@ -1,6 +1,13 @@
 def scheduling(meetHrs1,wrkHrs1, minTime1, meetHrs2, wrkHrs2, minTime2)-> list:
     mutual_hrs=[]
     rangeF = []
+    meetHrs1 = input()
+    wrkHrs1 = input()
+    minTime1 = input()
+    meetHrs2 = input()
+    wrkHrs2 = input()
+    minTime2 = input()
+    if meetHrs1 not in ran
     if meetHrs1 not in range(wrkHrs1) and meetHrs2 not in range(wrkHrs2):
         return ("error")
     else:
