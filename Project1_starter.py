c6156694d9add9be4a30466bad2f68e01e0d9d61
Douglasmyu <123704099+Douglasmyu@users.noreply.github.com>
2023-10-02 11:47:45 -0700

    Update Project1_starter.py

--- Project1_starter.py
+++ Project1_starter.py
@@ -1,32 +1,12 @@
 from datetime import timedelta
-
-
-def scheduling(meetHrs1,wrkHrs1, minTime1, meetHrs2, wrkHrs2, minTime2, meetLength)-> list:
-    mutual_hrs=[]
-    rangeF = []
-    meetHrs1 = input()
-    wrkHrs1 = input()
-    minTime1 = input()
-    meetHrs2 = input()
-    wrkHrs2 = input()
-    minTime2 = input()
+def scheduling(meetHrs1,wrkHrs1, meetHrs2, wrkHrs2, meetLength)-> list:
+    mutual_hrs =[]
     combined_schedule = [wrkHrs1, wrkHrs2]
     #check if meet hrs are in range of work hrs
     if meetHrs1 not in range(wrkHrs1) and meetHrs2 not in range(wrkHrs2):
         return ("error")
     else:
-        #use the lowest work hrs as range of possible meeting times
-        if wrkHrs1 <= wrkHrs2:
-            rangeF.append(wrkHrs1)
-        else:
-            rangeF.append(wrkHrs2)
         
-
-        testFiles = ["testCase1.txt", "testCase2.txt", "testCase3.txt"]
-        outputFiles = ["output1.txt", "output2.txt", "output3.txt" ]
-        for i in range(3):
-            with open(testFiles[i], 'a') as f:
-             f.write(' ')
         #find available work hrs
             for i in range(1, len(combined_schedule)):
                 start_time = max(combined_schedule[i-1][1], combined_schedule[i][0])
@@ -34,6 +14,13 @@ def scheduling(meetHrs1,wrkHrs1, minTime1, meetHrs2, wrkHrs2, minTime2, meetLeng
             
                 if end_time - start_time >= timedelta(minutes=meetLength):
                     mutual_hrs.append([start_time, end_time])
+            return mutual_hrs
 
-
+def main():
+    testFiles = ["input.txt"]
+    for i in range(1):
+            with open(testFiles[i], 'a') as f:
+             f.write(' ')
+             #for loop 10 times for 10 test cases
+                #for loop 5 times for each input case
 
